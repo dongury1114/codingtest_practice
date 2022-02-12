4d1467e2abb6583f12a831a2c70a46b8ca20708a
donggyu <dongury1114@gmail.com>
2022-02-12 23:24:36 +0900

    2022.02.12

--- b1330/b1330.js
+++ b1330/b1330.js
@@ -3,13 +3,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 input = input[0];
 input = input.split(" ").map((item) => +item);
-
-// console.log(input);
-
 solution(input[0], input[1]);
-
 function solution(A, B) {
-  const answer = 2 * B - A;
-
-  console.log(answer);
+  if (A > B) {
+    console.log(`>`);
+  } else if (A < B) {
+    console.log(`<`);
+  } else if (A === B) {
+    console.log(`==`);
+  }
 }
---
+++ b5339/b5339.js
@@ -0,0 +1,11 @@
+let answer = "";
+answer += `     /~\\\n`;
+answer += `    ( oo|\n`;
+answer += `    _\\=/_\n`;
+answer += `   /  _  \\\n`;
+answer += ` //|/.\\|\\\\\n`;
+answer += `============\n`;
+answer += `|          |\n`;
+answer += `|          |\n`;
+answer += `|          |`;
+console.log(answer);
