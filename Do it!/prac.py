ec790ec9e25263d62790a92586e97074a41d2bd1
donggyu <dongury1114@gmail.com>
2022-03-14 14:33:56 +0900

    2022.03.14

--- Do it!/prac.py
+++ Do it!/prac.py
@@ -1,4 +1,6 @@
-print('세 정수의 최댓값을 구합니다.')
-a = int(input('정수 a의 값을 입력하세요.:'))
-b = int(input('정수 b의 값을 입력하세요.:'))
-c = int(input('정수 c의 값을 입력하세요.:'))
+print ('이름을 입력하세요.:', end='')
+name = input()
+print(f'안녕하세요 {name} 님.')
+
+name = input('이름을 입력하세요.: ')
+print(f'안녕하세요 {name} 님.')
---
+++ b2588/b2588.py
@@ -0,0 +1,19 @@
+# a = int(input())
+# b = int(input())
+
+# A = a * (b % 10)
+# B = a * (b % 100 // 10)
+# C = a * (b // 100)
+
+# print(A)
+# print(B)
+# print(C)
+# print(a * b)
+
+A = input()
+print(type(A))
+print(A)
+print(int(A[0]))
+print(int(A[1]))
+print(type(int(A[0])))
+print(type(int(A[1])))
